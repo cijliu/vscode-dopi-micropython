0509d33156bac36615b38f9a1eac7d232203e617
cijliu <cijliu@qq.com>
2020-03-09 18:35:21 +0800

    optimize UI update command

--- src/modules/dopiUI.ts
+++ src/modules/dopiUI.ts
@@ -4,15 +4,18 @@ import {isConnect} from './serial'
 export function dopi_ui_update(dopiDataProvider:DopiProvider): vscode.Disposable{
     return (vscode.commands.registerCommand('dopi.ui.update', () => {
 		// The code you place here will be executed every time your command is executed
-        dopiDataProvider.connect = isConnect();
-		if(dopiDataProvider.connect){
-			dopiDataProvider.device.label = 'Device had connected';
-		}
-		else {
-			dopiDataProvider.device.label = 'Click to search serial';
-		}
-		dopiDataProvider.update();
-		vscode.window.registerTreeDataProvider("Dopi-export-node", dopiDataProvider);
+		setTimeout(()=>{
+			dopiDataProvider.connect = isConnect();
+			if(dopiDataProvider.connect){
+				dopiDataProvider.device.label = 'Device had connected';
+			}
+			else {
+				dopiDataProvider.device.label = 'Click to search serial';
+			}
+			dopiDataProvider.update();
+			vscode.window.registerTreeDataProvider("Dopi-export-node", dopiDataProvider);
+		},1000)
+		
 
 	}));
 }
